081ac350e5b7a88b8ba3409882c1dda514b1d4a0
Podosinova <mariya.podosinova@accenture.com>
2021-09-13 16:39:49 +0100

    set a timer

--- JS-Loader/index.js
+++ JS-Loader/index.js
@@ -1,11 +1,33 @@
 const btn = document.getElementById('btn')
 const loader = document.getElementById('loader')
+const timer = document.getElementById('timer')
+let interval
+let timeLeft = timer.innerText
 
 btn.addEventListener('click', load)
 
 function load() {
   loader.classList.toggle('loading')
-  loader.classList.contains('loading')
-    ? (btn.innerText = 'Pause')
-    : (btn.innerText = 'Load')
+  if (loader.classList.contains('loading')) {
+    btn.innerText = 'Pause'
+    countdown()
+  } else {
+    btn.innerText = 'Load'
+  }
+}
+
+function countdown() {
+  interval = setInterval(updateTimer, 1000)
+}
+
+function updateTimer() {
+  if (timeLeft > 1) {
+    timeLeft -= 1
+    timer.innerText = timeLeft
+  } else {
+    clearInterval(interval)
+    loader.classList.remove('loading')
+    timer.innerText = 0
+    btn.innerText = 'Load'
+  }
 }
