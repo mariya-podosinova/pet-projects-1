3332aedd6faf98f0fdae26a43ee103c27caac6d3
Podosinova <mariya.podosinova@accenture.com>
2021-09-15 16:00:27 +0100

    update with final pic, add animation for crown

--- JS-social-media/index.js
+++ JS-social-media/index.js
@@ -2,22 +2,27 @@ const capybaras = [
   {
     name: 'first',
     thumbnail: 'images/1.JPG',
+    best: true,
   },
   {
     name: 'second',
     thumbnail: 'images/2.JPG',
+    best: false,
   },
   {
     name: 'third',
     thumbnail: 'images/3.JPG',
+    best: false,
   },
   {
     name: 'fourth',
     thumbnail: 'images/4.JPG',
+    best: false,
   },
   {
     name: 'fifth',
     thumbnail: 'images/5.JPG',
+    best: false,
   },
 ]
 
@@ -27,6 +32,10 @@ capybaras.forEach((capybara) => {
   const card = document.createElement('div')
   card.classList.add('card')
   capybaraBasket.appendChild(card)
+
+  if (capybara.best) {
+    card.classList.add('best')
+  }
 
   const pic = document.createElement('img')
   pic.classList.add('pic')
